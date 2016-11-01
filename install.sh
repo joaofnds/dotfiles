4cb6f0615a5cc3108d82ea8702494ff755a24e4d
João Fernandes <joaovitorfernandes2@gmail.com>
2016-11-01 02:44:18 -0200

    Install.sh finally works

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
 dotfiles="$HOME/.dotfiles"
-
-echo "Home dir $HOME"
-echo "dotfiles dir $dotfiles"
+backupdir="$HOME/old_dotfiles"
 
 warn() {
 	echo "$1" >$2
@@ -15,17 +13,24 @@ die() {
 }
 
 lnif() {
-	if [ ! -e $2 ]; then
-		ln -sf $1 $1
+	if [ -e $2 ] && [ ! -h $2 ]; then
+		if [ ! -d $backupdir ]; then
+			echo "All your old dotfiles will be stored inside '$backupdir'"
+			mkdir $backupdir
+		fi
+		echo "Saving $2 inside '$backupdir'"
+		mv -f $2 $backupdir
 	fi
+	ln -fs $1 $2
 }
 
 echo "Installing/Updating dotfiles..."
 
 if [ ! -e $dotfiles/.git ]; then
-	echo "Cloning dotfile."
+	echo "Cloning dotfiles repo."
 	git clone https://github.com/joaofnds/dotfiles.git $dotfiles
 else
+	echo "Applying pull to dotfiles repo."
 	cd $dotfiles && git pull
 fi
 
@@ -41,15 +46,78 @@ lnif $dotfiles/.vimrc $HOME/.vimrc
 lnif $dotfiles/.vimrc_background $HOME/.vimrc_background
 lnif $dotfiles/.gvimrc $HOME/.gvimrc
 lnif $dotfiles/.vim $HOME/.vim
-git clone https://github.com/scrooloose/nerdtree.git $dotfiles/.vim/bundle/nerdtree
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git $dotfiles/.vim/bundle/nerdtree-git-plugin
-git clone https://github.com/tomtom/tlib_vim.git $dotfiles/.vim/bundle/tlib_vim
-git clone https://github.com/MarcWeber/vim-addon-mw-utils.git $dotfiles/.vim/bundle/vim-addon-mw-utils
-git clone git://github.com/tpope/vim-fugitive.git $dotfiles/.vim/bundle/vim-fugitive
-git clone https://github.com/xolox/vim-misc.git $dotfiles/.vim/bundle/vim-misc
-git clone https://github.com/xolox/vim-notes.git $dotfiles/.vim/bundle/vim-notes
-git clone https://github.com/garbas/vim-snipmate.git $dotfiles/.vim/bundle/vim-snipmate
-git clone git://github.com/tpope/vim-surround.git $dotfiles/.vim/bundle/vim-surround
+if [ -e $dotfiles/.vim/bundle/nerdtree ]; then
+	echo "Applying pull to NERDTree"
+	git --git-dir=$dotfiles/.vim/bundle/nerdtree/.git pull
+else
+	echo "Cloning NERDTree"
+	git clone https://github.com/scrooloose/nerdtree.git $dotfiles/.vim/bundle/nerdtree
+fi
+
+if [ -e $dotfiles/.vim/bundle/nerdtree-git-plugin ]; then
+	echo "Applying pull to NERDTree-git-plugin"
+	git --git-dir=$dotfiles/.vim/bundle/nerdtree-git-plugin/.git pull
+else
+	echo "Cloning NERDTree-git-git-plugin"
+	git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git $dotfiles/.vim/bundle/nerdtree-git-plugin
+fi
+
+if [ -e $dotfiles/.vim/bundle/tlib_vim ]; then
+	echo "Applying pull to tlib_vim"
+	git --git-dir=$dotfiles/.vim/bundle/tlib_vim/.git pull
+else
+	echo "Cloning tlib_vim"
+	git clone https://github.com/tomtom/tlib_vim.git $dotfiles/.vim/bundle/tlib_vim
+fi
+
+if [ -e $dotfiles/.vim/bundle/vim-addon-mw-utils ]; then
+	echo "Applying pull to vim-addon-mw-utils"
+	git --git-dir=$dotfiles/.vim/bundle/vim-addon-mw-utils/.git pull
+else
+	echo "Cloning vim-addon-mw-utils"
+	git clone https://github.com/MarcWeber/vim-addon-mw-utils.git $dotfiles/.vim/bundle/vim-addon-mw-utils
+fi
+
+if [ -e $dotfiles/.vim/bundle/vim-fugitive ]; then
+	echo "Applying pull to vim-fugitive"
+	git --git-dir=$dotfiles/.vim/bundle/vim-fugitive/.git pull
+else
+	echo "Cloning vim-fugitive"
+	git clone git://github.com/tpope/vim-fugitive.git $dotfiles/.vim/bundle/vim-fugitive
+fi
+
+if [ -e $dotfiles/.vim/bundle/vim-misc ]; then
+	echo "Applying pull to vim-misc"
+	git --git-dir=$dotfiles/.vim/bundle/vim-misc/.git pull
+else
+	echo "Cloning vim-misc"
+	git clone https://github.com/xolox/vim-misc.git $dotfiles/.vim/bundle/vim-misc
+fi
+
+if [ -e $dotfiles/.vim/bundle/vim-notes ]; then
+	echo "Applying pull to vim-notes"
+	git --git-dir=$dotfiles/.vim/bundle/vim-notes/.git pull
+else
+	echo "Cloning vim-notes"
+	git clone https://github.com/xolox/vim-notes.git $dotfiles/.vim/bundle/vim-notes
+fi
+
+if [ -e $dotfiles/.vim/bundle/vim-snipmate ]; then
+	echo "Applying pull to vim-snipmate"
+	git --git-dir=$dotfiles/.vim/bundle/vim-snipmate/.git pull
+else
+	echo "Cloning vim-snipmate"
+	git clone https://github.com/garbas/vim-snipmate.git $dotfiles/.vim/bundle/vim-snipmate
+fi
+
+if [ -e $dotfiles/.vim/bundle/vim-surround ]; then
+	echo "Applying pull to vim-surround"
+	git --git-dir=$dotfiles/.vim/bundle/vim-surround/.git pull
+else
+	echo "Cloning vim-surround"
+	git clone git://github.com/tpope/vim-surround.git $dotfiles/.vim/bundle/vim-surround
+fi
+
 
 
 # powerline-status
@@ -64,6 +132,9 @@ pip install --upgrade pip
 ## Installing powerline
 echo "Installing powerline"
 pip install --user powerline-status
+
+## Installing powerline fonts
+echo "Installing Inconsolata For Powerline font"
 wget https://github.com/powerline/fonts/blob/master/Inconsolata/Inconsolata%20for%20Powerline.otf
 
 ## Installing powerline fonts
@@ -114,7 +185,3 @@ lnif $dotfiles/.config/termite $HOME/.config/termite
 # git
 echo "Setting up gitconfig..."
 lnif $dotfiles/.gitconfig $HOME/.gitconfig
-
-# ssh
-echo "Setting up ssh public key..."
-lnif $dotfiles/.ssh $HOME/.ssh
