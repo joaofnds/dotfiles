71111d803cfe704e6d366231c42ab1edfc436132
João Fernandes <joaofnds@joaofnds.com>
2019-05-28 13:32:49 -0300

    [emacs] add font and tab config

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -1,3 +1,9 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+(set-default-font "Dank Mono-18")
+
+(setq tab-width 2
+      indent-tabs-mode nil)
+
+(setq make-backup-files nil)
