7dac2f31682d3f7994af6a13cf67064eda30eb77
João Fernandes <joaofnds@joaofnds.com>
2019-06-08 23:45:01 -0300

    [emacs] fix doom config

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -1,9 +1,10 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
-(set-default-font "Dank Mono-18")
+(setq
+  doom-font (font-spec :family "Dank Mono" :size 24)
+  doom-big-font (font-spec :family "Dank Mono" :size 30)
+  doom-theme 'solarized-dark
+  tab-width 2
+  indent-tabs-mode nil
+  make-backup-files nil)
 
-(setq tab-width 2
-      indent-tabs-mode nil)
-
-(setq make-backup-files nil)
--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -6,6 +6,4 @@
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
+(package! solarized-theme)
