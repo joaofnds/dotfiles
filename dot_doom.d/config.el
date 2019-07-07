a11e5bda852f140225132f4e6d843cee5597a22a
João Fernandes <joaofnds@joaofnds.com>
2019-07-07 14:27:24 -0300

    [emacs] use default theme. Add M-j M-k org keymaps

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -3,8 +3,11 @@
 (setq
   doom-font (font-spec :family "Dank Mono" :size 24)
   doom-big-font (font-spec :family "Dank Mono" :size 30)
-  doom-theme 'solarized-dark
   tab-width 2
   indent-tabs-mode nil
   make-backup-files nil)
 
+(after! org
+  (map! :map org-mode-map
+        :n "M-j" #'org-metadown
+        :n "M-k" #'org-metaup))
