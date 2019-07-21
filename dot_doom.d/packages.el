dd36b0837b7e5af3f1aed3f4d4eb72b075e8c68e
João Fernandes <joaofnds@joaofnds.com>
2019-07-21 00:50:21 -0300

    [emacs] add projectile-rails and evil-rails

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -1,11 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
-;; (package! builtin-package :disable t)
-
 (package! emamux)
 (package! emamux-ruby-test)
 (package! tmux-pane)
+(package! projectile-rails)
+(package! evil-rails)
