8684781fca01a60194bfa0cfddbb0250fe6ec4ce
João Fernandes <joaofnds@joaofnds.com>
2019-09-28 20:16:49 -0300

    [emacs] move doom config to .config. Remove modules

---
+++ dot_config/doom/packages.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; -*-
+;;; .doom.d/packages.el
+
+;;(package! emamux)
+;;(package! emamux-ruby-test)
+(package! tmux-pane)
+
+
+(package! projectile)
+(package! projectile-rails)
+
+;; Convert english words between singular and plural.
+;; Needed by projectile-rails
+(package! inflections)
--- dot_doom.d/packages.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; ~/.doom.d/packages.el
-
-(add-to-list 'load-path "~/.doom.d/vendor")
-
-(package! emamux)
-(package! emamux-ruby-test)
-(package! tmux-pane)
-(package! projectile-rails)
