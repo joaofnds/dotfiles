f237c009b9c5ac0f73c927aeb68d24ce54a64048
João Fernandes <joaofnds@joaofnds.com>
2020-01-05 18:23:16 -0300

    [hammerspoon] split event handlers
    Hopefully this makes it faster

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -9,25 +9,27 @@ end
 
 dragging_window = nil
 
-drag_event = hs.eventtap.new(
+move_window_event = hs.eventtap.new(
   {
     hs.eventtap.event.types.leftMouseDragged,
-    hs.eventtap.event.types.rightMouseDragged,
   }, function(e)
-    if not dragging_win then return nil end
-
     local dx = e:getProperty(hs.eventtap.event.properties.mouseEventDeltaX)
     local dy = e:getProperty(hs.eventtap.event.properties.mouseEventDeltaY)
-    local mouse = hs.mouse:getButtons()
 
-    if mouse.left then
-      dragging_win:move({dx, dy}, nil, false, 0)
-    elseif mouse.right then
-      local sz = dragging_win:size()
-      local w1 = sz.w + dx
-      local h1 = sz.h + dy
-      dragging_win:setSize(w1, h1)
-    end
+    dragging_win:move({dx, dy}, nil, false, 0)
+end)
+
+resize_window_event = hs.eventtap.new(
+  {
+    hs.eventtap.event.types.rightMouseDragged,
+  }, function(e)
+    local dx = e:getProperty(hs.eventtap.event.properties.mouseEventDeltaX)
+    local dy = e:getProperty(hs.eventtap.event.properties.mouseEventDeltaY)
+
+    local sz = dragging_win:size()
+    local w1 = sz.w + dx
+    local h1 = sz.h + dy
+    dragging_win:setSize(w1, h1)
 end)
 
 flag_event = hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(e)
@@ -35,10 +37,12 @@ flag_event = hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(
 
   if flags.cmd then
     dragging_win = get_window_under_mouse()
-    drag_event:start()
+    move_window_event:start()
+    resize_window_event:start()
   else
+    move_window_event:stop()
+    resize_window_event:stop()
     draggin_win = nil
-    drag_event:stop()
   end
 end)
 
