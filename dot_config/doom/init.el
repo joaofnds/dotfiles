f238b815de0927a7f141d44ca112be9a3f88c2d6
João Fernandes <joaofnds@joaofnds.com>
2020-02-12 00:08:51 -0300

    [emacs] enable web with html and css

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -163,7 +163,9 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
+       (web              ; the tubes
+        +html
+        +css)
 
        :email
        ;;(mu4e +gmail)
