63dbdf90ac386af56737894fb38f6371baca00fe
João Fernandes <joaofnds@joaofnds.com>
2020-04-17 00:05:38 -0300

    Update covid stats url

--- dot_scripts/executable_update-covid-cache.sh
+++ dot_scripts/executable_update-covid-cache.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-/usr/bin/curl -s https://corona.lmao.ninja/countries/brazil |
+/usr/bin/curl -s https://corona.lmao.ninja/v2/countries/brazil |
   /usr/local/bin/jq '["💀", .deaths, "(", .todayDeaths, ") 🦠", .active, "(", .todayCases, ")"] | join("")' |
   /usr/bin/tr -d "\"" > /Users/joaofnds/.cache/covid
