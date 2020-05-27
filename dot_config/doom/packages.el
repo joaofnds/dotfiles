4f59724310af67390ed12c8314f84caa6d206828
João Fernandes <joaofnds@joaofnds.com>
2020-05-27 08:01:56 -0300

    [emacs] add evil-numbers

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -3,3 +3,5 @@
 (package! magit-delta)  ; Use Delta when displaying diffs in Magit
 (package! tmux-pane)    ; Negivate seamlessly between tmux and emacs
 (package! evil-lion)    ; Evil align operator, port of vim-lion
+(package! evil-numbers) ; Increment and decrement numbers in Emacs
+
