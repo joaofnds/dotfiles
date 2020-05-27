de0bc288529ae695cc63ae022ebfc7282d5aaf95
João Fernandes <joaofnds@joaofnds.com>
2020-05-27 08:01:23 -0300

    Use for diffs

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -1,4 +1,5 @@
 ; -*- mode: emacs-lisp; no-byte-compile: t; -*-
 
-(package! tmux-pane) ; Negivate seamlessly between tmux and emacs
-(package! evil-lion) ; Evil align operator, port of vim-lion
+(package! magit-delta)  ; Use Delta when displaying diffs in Magit
+(package! tmux-pane)    ; Negivate seamlessly between tmux and emacs
+(package! evil-lion)    ; Evil align operator, port of vim-lion
