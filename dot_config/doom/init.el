bdc6fbf5b971d475d11df507ab1c117ce56fd2ab
João Fernandes <joaofnds@joaofnds.com>
2020-07-04 16:17:00 -0300

    [brew] use default chezmoi formula

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -1,5 +1,19 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+;; This file controls what Doom modules are enabled and what order they load in.
+;; Remember to run 'doom sync' after modifying it!
+
+;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
+;;      documentation. There you'll find information about all of Doom's modules
+;;      and what flags they support.
+
+;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
+;;      'C-c g k' for non-vim users) to view its documentation. This works on
+;;      flags as well (those symbols that start with a plus).
+;;
+;;      Alternatively, press 'gd' (or 'C-c g d') on a module to browse its
+;;      directory (for easy access to its source code).
+
 (doom! :input
        ;;chinese
        ;;japanese
--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -1,7 +1,20 @@
 ; -*- mode: emacs-lisp; no-byte-compile: t; -*-
 
 (package! magit-delta)  ; Use Delta when displaying diffs in Magit
+
 (package! tmux-pane)    ; Negivate seamlessly between tmux and emacs
 (package! evil-lion)    ; Evil align operator, port of vim-lion
 (package! evil-numbers) ; Increment and decrement numbers in Emacs
 
+;; (package! org-web-tools)    ; View, capture, and archive Web pages in Org-mode
+
+;; Ruby packages
+(package! inflections)      ; Needed by projectile-rails
+(package! projectile-rails) ; Minor mode for Rails projects based on projectile-mode
+(package! rspec-mode)
+(package! emamux)
+(package! emamux-ruby-test)
+
+;; (package! rjsx-mode)        ; Real support for JSX
+;; (package! macos :ignore (not (equal system-type 'darwin)))
+;; (package! eyebrowse)
