dc55aa35c380adc02839de1bd7912eac3f814b7e
João Fernandes <joaofnds@joaofnds.com>
2020-09-17 07:55:47 -0300

    Add prettier-js
    tags: [emacs]

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -2,6 +2,8 @@
 
 (package! magit-delta)  ; Use Delta when displaying diffs in Magit
 
+(package! prettier-js)  ; Use prettier to formar JavaScript files
+
 (package! tmux-pane)    ; Negivate seamlessly between tmux and emacs
 (package! evil-lion)    ; Evil align operator, port of vim-lion
 (package! evil-numbers) ; Increment and decrement numbers in Emacs
