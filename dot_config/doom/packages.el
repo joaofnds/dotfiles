304f5ae19c24916350ac71af4b445848a9e36fea
João Fernandes <joaofnds@joaofnds.com>
2020-11-24 05:27:32 -0300

    Add terraform-mode package
    :emacs:doom:

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -1,9 +1,6 @@
-; -*- mode: emacs-lisp; no-byte-compile: t; -*-
-
-(package! magit-delta)  ; Use Delta when displaying diffs in Magit
-
-(package! prettier-js)  ; Use prettier to formar JavaScript files
-
-(package! tmux-pane)    ; Negivate seamlessly between tmux and emacs
-(package! evil-lion)    ; Evil align operator, port of vim-lion
-(package! evil-numbers) ; Increment and decrement numbers in Emacs
+(package! magit-delta)    ; Use Delta when displaying diffs in Magit
+(package! prettier-js)    ; Use prettier to formar JavaScript files
+(package! terraform-mode) ; Major mode of terraform configuration file
+(package! tmux-pane)      ; Negivate seamlessly between tmux and emacs
+(package! evil-lion)      ; Evil align operator, port of vim-lion
+(package! evil-numbers)   ; Increment and decrement numbers in Emacs
