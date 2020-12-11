d84a0e6608679f6cbda2e2cacef14a01e502f8d5
João Fernandes <joaofnds@joaofnds.com>
2020-12-11 18:52:42 -0300

    Remove extra mail bindings
    :emacs:

--- dot_config/doom/mail-config.el
+++ dot_config/doom/mail-config.el
@@ -1,4 +1,3 @@
-
 (after! notmuch
   (setq message-send-mail-function   'smtpmail-send-it
         send-mail-function           'smtpmail-send-it
@@ -22,27 +21,3 @@
           (:name "sent" :query "tag:sent" :key "t")
           (:name "drafts" :query "tag:draft" :key "d")
           (:name "all mail" :query "*" :key "*"))))
-
-(defun notmuch-search-toggle-inbox ()
-  "Toggle deleted tag for message."
-  (interactive)
-  (evil-collection-notmuch-toggle-tag "inbox" "search"))
-
-(defun notmuch-search-toggle-archived ()
-  "Toggle deleted tag for message."
-  (interactive)
-  (evil-collection-notmuch-toggle-tag "archived" "search"))
-
-(defun notmuch-search-toggle-new ()
-  "Toggle deleted tag for message."
-  (interactive)
-  (evil-collection-notmuch-toggle-tag "new" "search"))
-
-(map! :map notmuch-search-mode
-      :leader
-      :prefix-map ("t" . "toggle")
-      :n "i" #'notmuch-search-toggle-inbox
-      :n "a" #'notmuch-search-toggle-archived
-      :n "n" #'notmuch-search-toggle-new
-      :n "u" #'evil-collection-notmuch-search-toggle-unread
-      :n "d" #'evil-collection-notmuch-search-toggle-delete)
