8ce17bcccfa5909b2fdeb1a1af8a73d1a2d3daa5
João Fernandes <joaofnds@joaofnds.com>
2020-12-12 13:05:12 -0300

    Set some org faces
    :emacs:

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -71,7 +71,29 @@
                            "~/notes/skore/skore.org"
                            "~/notes/ufpel/computer-lab/computer-lab.org"
                            "~/notes/ufpel/computer-theory/computer-theory.org"
-                           "~/notes/theo.org")))
+                           "~/notes/theo.org")
+
+        variable-pich-font-family "Iosevka Aile"
+        cursive-font-family "CMU Classical Serif")
+
+
+  (set-face-attribute 'variable-pitch nil :font "Iosevka Aile")
+
+  (set-face-attribute 'org-document-title nil :font variable-pich-font-family :height 1.5)
+  (set-face-attribute 'org-document-info nil :font variable-pich-font-family :height 1.2)
+  (set-face-attribute 'org-document-info-keyword nil :height 0.9 :inherit 'shadow)
+  (set-face-attribute 'org-meta-line nil :height 0.9 :inherit 'shadow)
+  (set-face-attribute 'org-quote nil :font cursive-font-family :height 1.2 :inherit 'org-quote)
+  (set-face-attribute 'org-todo nil :weight 'ultrabold)
+  (set-face-attribute 'org-tag nil :font variable-pich-font-family)
+  (set-face-attribute 'org-level-1 nil :font variable-pich-font-family :height 1.0 :inherit 'outline-1)
+  (set-face-attribute 'org-level-2 nil :font variable-pich-font-family :height 1.0 :inherit 'outline-2)
+  (set-face-attribute 'org-level-3 nil :font variable-pich-font-family :height 1.0 :inherit 'outline-3)
+  (set-face-attribute 'org-level-4 nil :font variable-pich-font-family :height 1.0 :inherit 'outline-4)
+  (set-face-attribute 'org-level-5 nil :font variable-pich-font-family :height 1.0 :inherit 'outline-5)
+  (set-face-attribute 'org-level-6 nil :font variable-pich-font-family :height 1.0 :inherit 'outline-6)
+  (set-face-attribute 'org-level-7 nil :font variable-pich-font-family :height 1.0 :inherit 'outline-7)
+  (set-face-attribute 'org-level-8 nil :font variable-pich-font-family :height 1.0 :inherit 'outline-8))
 
 (add-hook! '(go-mode-hook js2-mode-hook enh-ruby-mode-hook)
            #'(hs-minor-mode lsp-deferred yas-minor-mode))
