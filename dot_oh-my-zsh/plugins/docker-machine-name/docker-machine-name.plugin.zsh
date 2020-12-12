1a5b94151ab5cf2f98b21a345691a1cc10c658a5
João Fernandes <joaofnds@joaofnds.com>
2020-12-12 10:31:46 -0300

    Remove custom zsh theme
    :zsh:

--- dot_oh-my-zsh/plugins/docker-machine-name/docker-machine-name.plugin.zsh
+++
@@ -1,5 +0,0 @@
-function docker_machine_prompt_info() {
-  if [ -n "$DOCKER_MACHINE_NAME" ]; then
-    echo "%{$fg_bold[blue]%}docker:(%{$fg[red]%}$DOCKER_MACHINE_NAME%{$fg[blue]%})%{$reset_color%} "
-  fi
-}
