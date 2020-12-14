c89b262afe1d4baa13150ed9e4930e484d9d293f
João Fernandes <joaofnds@joaofnds.com>
2020-12-14 13:38:37 -0300

    Add spotlight-ignore-node
    :scripts:

---
+++ dot_scripts/executable_spotlight-ignore-node.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+sudo /usr/libexec/PlistBuddy -c "Delete :Exclusions" /System/Volumes/Data/.Spotlight-V100/VolumeConfiguration.plist
+sudo /usr/libexec/PlistBuddy -c "Add :Exclusions array" /System/Volumes/Data/.Spotlight-V100/VolumeConfiguration.plist
+
+sudo fd \
+  --type directory \
+  --fixed-strings \
+  --absolute-path \
+  --prune \
+  node_modules \
+  --exec PlistBuddy \
+    -c "Add :Exclusions: string {}" \
+    /System/Volumes/Data/.Spotlight-V100/VolumeConfiguration.plist
+
+sudo launchctl stop com.apple.metadata.mds
+sudo launchctl start com.apple.metadata.mds
