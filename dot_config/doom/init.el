8971d1610f0238fdbec23678e210f42b37a8943a
João Fernandes <joaofnds@joaofnds.com>
2020-12-14 00:52:01 -0300

    Add hunspell flag to spell module
    :emacs:doom:

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -38,7 +38,7 @@
 
        :checkers
        syntax            ; tasing you for every semicolon you forget
-       spell             ; tasing you for misspelling mispelling
+       (spell +hunspell) ; tasing you for misspelling mispelling
 
        :tools
        docker
