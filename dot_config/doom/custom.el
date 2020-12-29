fa8988b11067d0ba7f90a11e12c6c549348ac5ce
João Fernandes <joaofnds@joaofnds.com>
2020-12-29 20:39:37 -0300

    Update elfeed-feeds
    :emacs:

--- dot_config/doom/custom.el
+++ dot_config/doom/custom.el
@@ -4,41 +4,42 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(elfeed-feeds
-   '("https://cate.blog/feed/"
-     ("https://www.with-emacs.com/rss.xml")
-     ("https://www.rubytapas.com/feed/")
-     ("https://www.ruby-lang.org/en/feeds/news.rss")
-     ("https://www.pipefy.com/feed/")
-     ("https://www.intercom.com/blog/engineering/feed/")
-     ("https://scotch.io/feed")
-     ("https://oremacs.com/atom.xml")
-     ("https://medium.com/feed/netflix-techblog")
-     ("https://medium.com/feed/discord-engineering/tagged/engineering")
-     ("https://medium.com/feed/building-nubank")
-     ("https://labs.spotify.com/feed/")
-     ("https://jvns.ca/atom.xml")
-     ("https://github.blog/engineering.atom")
-     ("https://feeds.feedburner.com/philipwalton")
-     ("https://feeds.feedburner.com/GiantRobotsSmashingIntoOtherGiantRobots")
-     ("https://feeds.feedburner.com/2ality")
-     ("https://evilmartians.com/chronicles.atom")
-     ("https://engineering.wework.com/feed")
-     ("https://developers.google.com/web/updates/rss.xml")
-     ("https://dassur.ma/index.xml")
-     ("https://daringfireball.net/feeds/main")
-     ("https://cate.blog/feed/")
-     ("https://blogs.dropbox.com/tech/feed/")
-     ("https://blog.heroku.com/engineering/feed")
-     ("https://blog.gopheracademy.com/index.xml")
-     ("https://blog.golang.org/feed.atom")
-     ("https://blog.cloudflare.com/rss/")
-     ("https://blog.bigbinary.com/feed.xml")
-     ("https://blog.appsignal.com/feed.xml")
-     ("http://research.swtch.com/feed.atom")
-     ("http://pragmaticemacs.com/feed/")
-     ("http://feeds.feedburner.com/codinghorror")
-     ("http://feeds.feedburner.com/CssTricks")
-     ("http://blog.plataformatec.com.br/feed/")))
+   '("http://blog.plataformatec.com.br/feed/"
+     "http://feeds.feedburner.com/CssTricks"
+     "http://feeds.feedburner.com/codinghorror"
+     "http://pragmaticemacs.com/feed/"
+     "http://research.swtch.com/feed.atom"
+     "https://betweentwoparens.com/rss.xml"
+     "https://blog.appsignal.com/feed.xml"
+     "https://blog.bigbinary.com/feed.xml"
+     "https://blog.cloudflare.com/rss/"
+     "https://blog.golang.org/feed.atom"
+     "https://blog.gopheracademy.com/index.xml"
+     "https://blog.heroku.com/engineering/feed"
+     "https://blogs.dropbox.com/tech/feed/"
+     "https://cate.blog/feed/"
+     "https://daringfireball.net/feeds/main"
+     "https://dassur.ma/index.xml"
+     "https://developers.google.com/web/updates/rss.xml"
+     "https://engineering.wework.com/feed"
+     "https://evilmartians.com/chronicles.atom"
+     "https://feeds.feedburner.com/2ality"
+     "https://feeds.feedburner.com/GiantRobotsSmashingIntoOtherGiantRobots"
+     "https://feeds.feedburner.com/philipwalton"
+     "https://github.blog/engineering.atom"
+     "https://jvns.ca/atom.xml"
+     "https://labs.spotify.com/feed/"
+     "https://medium.com/feed/building-nubank"
+     "https://medium.com/feed/discord-engineering/tagged/engineering"
+     "https://medium.com/feed/netflix-techblog"
+     "https://oremacs.com/atom.xml"
+     "https://practicalli.github.io/blog/feed.xml"
+     "https://scotch.io/feed"
+     "https://www.intercom.com/blog/engineering/feed/"
+     "https://www.pipefy.com/feed/"
+     "https://www.ruby-lang.org/en/feeds/news.rss"
+     "https://www.rubytapas.com/feed/"
+     "https://www.with-emacs.com/rss.xml"))
  '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
