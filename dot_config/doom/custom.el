5c1d451fcf4747521974c38731e8d5eec3e9b49c
João Fernandes <joaofnds@joaofnds.com>
2020-12-28 22:39:36 -0300

    Add custom.el with elfeed feeds
    :emacs:

---
+++ dot_config/doom/custom.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("https://cate.blog/feed/"
+     ("https://www.with-emacs.com/rss.xml")
+     ("https://www.rubytapas.com/feed/")
+     ("https://www.ruby-lang.org/en/feeds/news.rss")
+     ("https://www.pipefy.com/feed/")
+     ("https://www.intercom.com/blog/engineering/feed/")
+     ("https://scotch.io/feed")
+     ("https://oremacs.com/atom.xml")
+     ("https://medium.com/feed/netflix-techblog")
+     ("https://medium.com/feed/discord-engineering/tagged/engineering")
+     ("https://medium.com/feed/building-nubank")
+     ("https://labs.spotify.com/feed/")
+     ("https://jvns.ca/atom.xml")
+     ("https://github.blog/engineering.atom")
+     ("https://feeds.feedburner.com/philipwalton")
+     ("https://feeds.feedburner.com/GiantRobotsSmashingIntoOtherGiantRobots")
+     ("https://feeds.feedburner.com/2ality")
+     ("https://evilmartians.com/chronicles.atom")
+     ("https://engineering.wework.com/feed")
+     ("https://developers.google.com/web/updates/rss.xml")
+     ("https://dassur.ma/index.xml")
+     ("https://daringfireball.net/feeds/main")
+     ("https://cate.blog/feed/")
+     ("https://blogs.dropbox.com/tech/feed/")
+     ("https://blog.heroku.com/engineering/feed")
+     ("https://blog.gopheracademy.com/index.xml")
+     ("https://blog.golang.org/feed.atom")
+     ("https://blog.cloudflare.com/rss/")
+     ("https://blog.bigbinary.com/feed.xml")
+     ("https://blog.appsignal.com/feed.xml")
+     ("http://research.swtch.com/feed.atom")
+     ("http://pragmaticemacs.com/feed/")
+     ("http://feeds.feedburner.com/codinghorror")
+     ("http://feeds.feedburner.com/CssTricks")
+     ("http://blog.plataformatec.com.br/feed/")))
+ '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
