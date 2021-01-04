b7897672b6ce1bab94857ed6e9038148fcfc2be2
João Fernandes <joaofnds@joaofnds.com>
2021-01-04 16:38:23 -0300

    Remove doom-dashboard and modeline modules
    :emacs:doom:

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -6,9 +6,7 @@
 
        :ui
        doom               ; what makes DOOM look the way it does
-       doom-dashboard     ; a nifty splash screen for Emacs
        hl-todo            ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       modeline           ; snazzy, Atom-inspired modeline, plus API
        ophints            ; highlight the region an operation acts on
        (popup +defaults)  ; tame sudden yet inevitable temporary windows
        vc-gutter          ; vcs diff in the fringe
