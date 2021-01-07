9a6c34bbff34ee2f2a995679dc938f1c71bc5ac3
João Fernandes <joaofnds@joaofnds.com>
2021-01-06 23:46:05 -0300

    Move neotree config to its own file
    :emacs:

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -66,30 +66,9 @@
       (setq cider-clojure-cli-global-options cider-options-with-rebl)
     (setq cider-clojure-cli-global-options default-cider-options)))
 
-(defun neotree-move-node ()
-  "Move current node to another directory."
-  (interactive)
-  (if-let* ((current-path (neo-buffer--get-filename-current-line))
-            (to-path (read-file-name (format "Move %s to:" (f-filename current-path)))))
-      (if (f-directory-p to-path)
-          (f-move current-path to-path)
-        (rename-file current-path to-path)))
-  (neo-buffer--refresh t))
-
-(defalias 'neotree-add-node 'neotree-create-node)
-
-(map! :map neotree-mode-map
-      :n "m a" #'neotree-add-node
-      :n "m m" #'neotree-move-node
-      :n "m d" #'neotree-delete-node
-      :n "m c" #'neotree-copy-node
-
-      :n "A" #'neotree-stretch-toggle
-      :n "i" #'neotree-enter-horizontal-split
-      :n "s" #'neotree-enter-vertical-split)
-
 (defun after-doom-loaded ()
   (load "org-config")
+  (load "neotree-config")
 
   (require 'tmux-pane)
   (require 'tree-sitter)
---
+++ dot_config/doom/neotree-config.el
@@ -0,0 +1,21 @@
+(defun neotree-move-node ()
+  "Move current node to another directory."
+  (interactive)
+  (if-let* ((current-path (neo-buffer--get-filename-current-line))
+            (to-path (read-file-name (format "Move %s to:" (f-filename current-path)))))
+      (if (f-directory-p to-path)
+          (f-move current-path to-path)
+        (rename-file current-path to-path)))
+  (neo-buffer--refresh t))
+
+(defalias 'neotree-add-node 'neotree-create-node)
+
+(map! :map neotree-mode-map
+      :n "m a" #'neotree-add-node
+      :n "m m" #'neotree-move-node
+      :n "m d" #'neotree-delete-node
+      :n "m c" #'neotree-copy-node
+
+      :n "A" #'neotree-stretch-toggle
+      :n "i" #'neotree-enter-horizontal-split
+      :n "s" #'neotree-enter-vertical-split)
