c06036383e474cdf91a3b86c96e8f5e587597b46
João Fernandes <joaofnds@joaofnds.com>
2021-01-12 06:49:32 -0300

    Increase elfeed-search-filter to 5 years
    :emacs:

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -41,7 +41,7 @@
 (setq-hook! 'ruby-mode-hook +format-with-lsp t)
 
 (after! elfeed
-  (setq-default elfeed-search-filter "@50-months-ago +unread"))
+  (setq-default elfeed-search-filter "@5-years-ago +unread"))
 
 (map!
  "C-s" #'swiper
