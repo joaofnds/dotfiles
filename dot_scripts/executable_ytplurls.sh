0c1d21643a9b5e20e1950ed5d8f23e934216accb
João Fernandes <joaofnds@joaofnds.com>
2021-03-28 12:47:04 -0300

    Fix ytplurls
    :scripts:

--- dot_scripts/executable_ytplurls.sh
+++ dot_scripts/executable_ytplurls.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-yt_data=$(curl -s $1 | grep  'ytInitialData')
-yt_data="${yt_data##*= }"
+yt_data=$(curl -s $1)
+yt_data="${yt_data##*var ytInitialData = }"
 yt_data="${yt_data%%;*}"
 
 JQ_EACH_VIDEO='.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents[].playlistVideoRenderer'
-JQ_URL_AND_TITLE="$JQ_EACH_VIDEO | \"https://youtube.com\" + .navigationEndpoint.commandMetadata.webCommandMetadata.url + \"\t\" + .title.simpleText"
+JQ_URL_AND_TITLE="$JQ_EACH_VIDEO | \"https://youtube.com\" + .navigationEndpoint.commandMetadata.webCommandMetadata.url + \"\t\" + .title.runs[0].text"
 
 jq -r "$JQ_URL_AND_TITLE" <<< "$yt_data"
