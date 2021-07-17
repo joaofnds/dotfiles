a313d78d79d1675ae5cb86d1a6e925dae30ba24f
João Fernandes <joaofnds@joaofnds.com>
2021-07-17 17:03:02 -0300

    remove evil lion, doom already includes it
    :emacs:

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -4,7 +4,6 @@
 (package! prettier-js)       ; Use prettier to formar JavaScript files
 (package! terraform-mode)    ; Major mode of terraform configuration file
 (package! tmux-pane)         ; Negivate seamlessly between tmux and emacs
-(package! evil-lion)         ; Evil align operator, port of vim-lion
 (package! evil-numbers)      ; Increment and decrement numbers in Emacs
 (package! tree-sitter)       ; Parser generator tool and an incremental parsing library
 (package! tree-sitter-langs) ; Populates global registries of grammars and queries
