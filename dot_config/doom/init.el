1d2937872be18f47488e0c927f5612cf5e362c85
João Fernandes <joaofnds@joaofnds.com>
2021-07-18 20:50:43 -0300

    enable chicken and guile
    :emacs:doom:

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -58,7 +58,9 @@
         +pandoc)
        ;; (python +lsp)      ; beautiful is better than ugly
        ;; (ruby +lsp +rails) ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       scheme             ; a fully conniving family of lisps
+       (scheme            ; a fully conniving family of lisps
+        +chicken
+        +guile)
        (sh +lsp)          ; she sells {ba,z,fi}sh shells on the C xor
        web                ; the tubes
        yaml               ; JSON, but readable
