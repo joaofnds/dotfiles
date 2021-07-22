869d7c81e4000f2029be2ef438e99c7e6f304da9
João Fernandes <joaofnds@joaofnds.com>
2021-07-21 21:51:30 -0300

    add dash, s and f
    :emacs:

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -6,6 +6,9 @@
 (package! tree-sitter)       ; Parser generator tool and an incremental parsing library
 (package! tree-sitter-langs) ; Populates global registries of grammars and queries
 (package! string-inflection) ; underscore -> UPCASE -> CamelCase -> lowerCamelCase conversion of names
+(package! dash)              ; a modern list library for emacs
+(package! s)                 ; the long lost emacs string manipulation library.
+(package! f)                 ; modern api for working with files and directories
 
 (disable-packages!
  haml-mode
