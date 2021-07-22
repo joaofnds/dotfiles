0835874763272bbd23887631d42bfceb71c9f77a
João Fernandes <joaofnds@joaofnds.com>
2021-07-21 21:47:26 -0300

    remove evil-numbers
    :emacs:

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -5,7 +5,6 @@
 (package! prettier-js)       ; Use prettier to formar JavaScript files
 (package! terraform-mode)    ; Major mode of terraform configuration file
 (package! tmux-pane)         ; Negivate seamlessly between tmux and emacs
-(package! evil-numbers)      ; Increment and decrement numbers in Emacs
 (package! tree-sitter)       ; Parser generator tool and an incremental parsing library
 (package! tree-sitter-langs) ; Populates global registries of grammars and queries
 (package! php-mode)          ; How would you know it's a variable if the dollar wasn't there, right?
