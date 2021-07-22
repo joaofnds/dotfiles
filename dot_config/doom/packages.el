49176b9ce691b294bd1a332dd72c4cacceebe3ca
João Fernandes <joaofnds@joaofnds.com>
2021-07-21 21:51:58 -0300

    remove lines and update comment
    :emacs:

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -1,14 +1,14 @@
 ;; -*- lexical-binding: t; -*-
 
 (unpin! format-all)
-(package! magit-delta)       ; Use Delta when displaying diffs in Magit
-(package! tmux-pane)         ; Negivate seamlessly between tmux and emacs
-(package! tree-sitter)       ; Parser generator tool and an incremental parsing library
-(package! tree-sitter-langs) ; Populates global registries of grammars and queries
-(package! string-inflection) ; underscore -> UPCASE -> CamelCase -> lowerCamelCase conversion of names
 (package! dash)              ; a modern list library for emacs
 (package! s)                 ; the long lost emacs string manipulation library.
 (package! f)                 ; modern api for working with files and directories
+(package! magit-delta)       ; use delta when displaying diffs in magit
+(package! tmux-pane)         ; negivate seamlessly between tmux and emacs
+(package! tree-sitter)       ; parser generator tool and an incremental parsing library
+(package! tree-sitter-langs) ; populates global registries of grammars and queries
+(package! string-inflection) ; underscore_case -> UPCASE -> camelCase -> PascalCase conversion of names
 
 (disable-packages!
  haml-mode
