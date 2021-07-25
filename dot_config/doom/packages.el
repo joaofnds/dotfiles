a1b5227924fd629c805e52c7d80626ed39c09967
João Fernandes <joaofnds@joaofnds.com>
2021-07-25 14:27:44 -0300

    remove magit-delta
    :emacs:

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -3,15 +3,14 @@
 (package! dash)              ; a modern list library for emacs
 (package! s)                 ; the long lost emacs string manipulation library.
 (package! f)                 ; modern api for working with files and directories
-(package! magit-delta)       ; use delta when displaying diffs in magit
 (package! tmux-pane)         ; negivate seamlessly between tmux and emacs
 (package! tree-sitter)       ; parser generator tool and an incremental parsing library
 (package! tree-sitter-langs) ; populates global registries of grammars and queries
 (package! string-inflection) ; underscore_case -> UPCASE -> camelCase -> PascalCase conversion of names
 (package! org-roam)          ; rudimentary roam replica with org-mode
 (package! package-lint)      ; a linting library for elisp package authors
-(package! emacs-tmux-runner  ; my port of christoomey/vim-tmux-runner
-  :recipe (:host github :repo "joaofnds/emacs-tmux-runner"))
+(package! emacs-tmux-runner ; my port of christoomey/vim-tmux-runner
+  :recipe (:host github :repo "joaofnds/emacs-tmux-runner")
 
 (unpin! format-all)
 
