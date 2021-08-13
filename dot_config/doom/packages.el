f0feb769e8d9d9f3fae26e1aa360f803456d40c5
João Fernandes <joaofnds@joaofnds.com>
2021-08-13 06:47:41 -0300

    reorganize packages.el
    :emacs:

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -1,20 +1,28 @@
 ;; -*- lexical-binding: t; -*-
 
-(package! flymake-grammarly)
 (package! dash)              ; a modern list library for emacs
-(package! s)                 ; the long lost emacs string manipulation library.
+(package! s)                 ; the long lost emacs string manipulation library
 (package! f)                 ; modern api for working with files and directories
-(package! tmux-pane)         ; negivate seamlessly between tmux and emacs
+
+(package! flymake-grammarly) ; flymake support for grammarly
+
+
+
 (package! tree-sitter)       ; parser generator tool and an incremental parsing library
 (package! tree-sitter-langs) ; populates global registries of grammars and queries
-(package! string-inflection) ; underscore_case -> UPCASE -> camelCase -> PascalCase conversion of names
-(package! org-roam)          ; rudimentary roam replica with org-mode
-(package! package-lint)      ; a linting library for elisp package authors
-(package! emacs-tmux-runner ; my port of christoomey/vim-tmux-runner
-  :recipe (:host github :repo "joaofnds/emacs-tmux-runner"))
 
+(package! org-roam)          ; rudimentary roam replica with org-mode
 (package! websocket)         ; org-roam-ui dependency
-(package! org-roam-ui
-  :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
+(package! org-roam-ui        ; a graphical frontend for exploring org-roam
+  :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out"))
+  :requires websocket)
+
+(package! string-inflection) ; underscore_case -> UPCASE -> camelCase -> PascalCase conversion of names
+(package! package-lint)      ; a linting library for elisp package authors
+
+(package! tmux-pane)         ; negivate seamlessly between tmux and emacs
+(package! emacs-tmux-runner  ; my port of christoomey/vim-tmux-runner
+  :recipe (:local-repo "~/code/emacs-tmux-runner"
+           :build (:not compile)))
 
 (unpin! format-all)
