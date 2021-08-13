e76588d0c854a16aa8886b1d63413540c05db309
João Fernandes <joaofnds@joaofnds.com>
2021-08-13 07:09:40 -0300

    remove `:requires` from `package!`
    this is an `use-package` option
    
    :emacs:

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -14,8 +14,7 @@
 (package! org-roam)          ; rudimentary roam replica with org-mode
 (package! websocket)         ; org-roam-ui dependency
 (package! org-roam-ui        ; a graphical frontend for exploring org-roam
-  :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out"))
-  :requires websocket)
+  :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
 
 (package! string-inflection) ; underscore_case -> UPCASE -> camelCase -> PascalCase conversion of names
 (package! package-lint)      ; a linting library for elisp package authors
