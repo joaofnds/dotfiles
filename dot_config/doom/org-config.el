032a0815108c8090db5247a9631e10cf85b6be34
João Fernandes <joaofnds@joaofnds.com>
2021-08-16 06:57:54 -0300

    add all org files to org-agenda
    this is probably cause performance problemns later on (due to the number
    o files), but for now it's working good.
    
    :emacs:org:

--- dot_config/doom/org-config.el
+++ dot_config/doom/org-config.el
@@ -22,12 +22,8 @@
       org-log-done 'time
       org-log-into-drawer t
       org-format-latex-options (plist-put org-format-latex-options :scale 2.5)
-
-      org-agenda-files '("~/notes/home.org"
-                         "~/notes/habits.org"
-                         "~/notes/ufpel/ufpel.org"
-                         "~/notes/melhor-envio/melhor-envio.org"
-                         "~/notes/melhor-envio/melhor-rastreio.org"))
+      org-agenda-file-regexp "\\`[^.].*\\.org\\'"
+      org-agenda-files '("~/notes"))
 
 (set-face-attribute 'org-block-begin-line nil :font jf/font/fixed-family :height 0.8 :background nil :inherit 'shadow)
 (set-face-attribute 'org-block-end-line nil :font jf/font/fixed-family :height 0.8 :background nil :inherit 'shadow)
