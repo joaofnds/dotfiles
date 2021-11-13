3e4edb7a0c9069bd6fa92da5c753126ef22f0a8c
João Fernandes <joaofnds@joaofnds.com>
2021-11-13 16:47:53 -0300

    make reload-known-projects load only git repos
    also update melhor-envio path

--- dot_config/doom/jf.el
+++ dot_config/doom/jf.el
@@ -4,12 +4,33 @@
     ("true" (load-theme jf/dark-theme))
     ("false" (load-theme jf/light-theme))))
 
+(defun some-prefix (str &rest prefixes)
+  (--some (s-starts-with? it str) prefixes))
+
+(defun remove-known-projects-starting-with (&rest prefixes)
+  (setf projectile-known-projects
+        (--remove
+         (apply 'some-prefix it prefixes)
+         projectile-known-projects)))
+
+(defun git-repo? (path)
+  (f-dir? (f-join path ".git")))
+
+(defun repos-from-path (path)
+  (-filter 'git-repo? (f-directories path)))
+
 (defun jf/reload-known-projects ()
-  (-each
-      (-concat
-       '("~/notes" "~/code/dotfiles" "~/code/exercism")
-       (f-directories "~/code/melhor-envio"))
-    'projectile-add-known-project))
+  (let ((me-path "~/code/me")
+        (mr-path "~/code/me/mr"))
+
+    (remove-known-projects-starting-with me-path)
+
+    (-each
+        (-concat
+         '("~/notes" "~/code/dotfiles" "~/code/exercism")
+         (repos-from-path me-path)
+         (repos-from-path mr-path))
+      'projectile-add-known-project)))
 
 (defun jf/restore-frame-size ()
   (set-frame-size
