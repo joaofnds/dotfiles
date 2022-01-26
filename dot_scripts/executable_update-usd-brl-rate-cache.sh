9ee58cb666cafa257ae31db50218c420d3563459
João Fernandes <joaofnds@joaofnds.com>
2022-01-25 23:36:39 -0300

    set precision to 2

--- dot_scripts/executable_update-usd-brl-rate-cache.sh
+++ dot_scripts/executable_update-usd-brl-rate-cache.sh
@@ -7,4 +7,4 @@ source "$HOME/.hardware.env"
 RESPONSE=$(curl -s "http://api.exchangeratesapi.io/v1/latest?access_key=$EXCHANGERATESAPI_TOKEN")
 USD=$(jq -r '.rates.USD' <<< $RESPONSE)
 BRL=$(jq -r '.rates.BRL' <<< $RESPONSE)
-bc -l <<< "1.0 / $USD * $BRL" | awk '{ printf("R$%.4f", $0) }' > $HOME/.cache/usd-brl-rate
+bc -l <<< "1.0 / $USD * $BRL" | awk '{ printf("R$%.2f", $0) }' > $HOME/.cache/usd-brl-rate
--- dot_scripts/executable_update-xlm-usd-rate-cache.sh
+++ dot_scripts/executable_update-xlm-usd-rate-cache.sh
@@ -2,4 +2,4 @@
 
 HOME=$(eval echo "~$joaofnds")
 
-curl -s "rate.sx/1xlm" | awk '{ printf("$%.4f", $0) }' > $HOME/.cache/xlm-usd-rate
+curl -s "rate.sx/1xlm" | awk '{ printf("$%.2g", $0) }' > $HOME/.cache/xlm-usd-rate
