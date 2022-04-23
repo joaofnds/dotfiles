0206cc97ebe26dfabe5aa1bf1cbe4b1194cee155
João Fernandes <joaofnds@joaofnds.com>
2022-04-23 15:15:29 -0300

    inline script

--- dot_scripts/executable_update-btc-usd-rate-cache.sh
+++ dot_scripts/executable_update-btc-usd-rate-cache.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env sh
 
-HOME=$(eval echo "~$joaofnds")
-outfile=$HOME/.cache/btc-usd-rate
-
-rate=$(curl -sf "rate.sx/1btc")
-if [[ $? = 0 ]]; then
-    awk '{ printf("$%.1fk", $0/1000) }' <<< $rate > $outfile
-else
-    printf '' > $outfile
-fi
+curl -sf "rate.sx/1btc" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/btc-usd-rate"
--- dot_scripts/executable_update-xlm-usd-rate-cache.sh
+++ dot_scripts/executable_update-xlm-usd-rate-cache.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env sh
 
-HOME=$(eval echo "~$joaofnds")
-outfile=$HOME/.cache/xlm-usd-rate
-
-rate=$(curl -sf "rate.sx/1xlm")
-if [[ $? = 0 ]]; then
-    awk '{ printf("$%.1fk", $0/1000) }' <<< $rate > $outfile
-else
-    printf '' > $outfile
-fi
+curl -sf "rate.sx/1xlm" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/xlm-usd-rate"
