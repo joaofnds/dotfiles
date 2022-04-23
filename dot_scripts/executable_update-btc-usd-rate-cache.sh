d477341b48861b36ff2319a0a69f7323bc330041
João Fernandes <joaofnds@joaofnds.com>
2022-04-23 14:20:01 -0300

    remove output when curl fails

--- dot_scripts/executable_update-btc-usd-rate-cache.sh
+++ dot_scripts/executable_update-btc-usd-rate-cache.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
 HOME=$(eval echo "~$joaofnds")
+outfile=$HOME/.cache/btc-usd-rate
 
-curl -s "rate.sx/1btc" | awk '{ printf("$%.1fk", $0/1000) }' > $HOME/.cache/btc-usd-rate
+rate=$(curl -sf "rate.sx/1btc")
+if [[ $? = 0 ]]; then
+    awk '{ printf("$%.1fk", $0/1000) }' <<< $rate > $outfile
+else
+    printf '' > $outfile
+fi
--- dot_scripts/executable_update-xlm-usd-rate-cache.sh
+++ dot_scripts/executable_update-xlm-usd-rate-cache.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
 HOME=$(eval echo "~$joaofnds")
+outfile=$HOME/.cache/xlm-usd-rate
 
-curl -s "rate.sx/1xlm" | awk '{ printf("$%.2g", $0) }' > $HOME/.cache/xlm-usd-rate
+rate=$(curl -sf "rate.sx/1xlm")
+if [[ $? = 0 ]]; then
+    awk '{ printf("$%.1fk", $0/1000) }' <<< $rate > $outfile
+else
+    printf '' > $outfile
+fi
