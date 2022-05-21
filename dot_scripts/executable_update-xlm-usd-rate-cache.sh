bdc0fe251f4f49ee7cfcaaa5d0f6825403dc41fa
João Fernandes <joaofnds@joaofnds.com>
2022-05-21 01:13:15 -0300

    fix xlm rate formatting

--- dot_scripts/executable_update-xlm-usd-rate-cache.sh
+++ dot_scripts/executable_update-xlm-usd-rate-cache.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-curl -sf "rate.sx/1xlm" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/xlm-usd-rate"
+curl -sf "rate.sx/1xlm" | awk '{ printf("$%.2f", $0) }' >"$HOME/.cache/xlm-usd-rate"
