786ea6edb8311cd1fd82cc9143ad69e77c0ff1bb
João Fernandes <joaofnds@joaofnds.com>
2022-08-21 11:20:01 -0300

    join cripto rates update script

--- dot_scripts/executable_update-btc-usd-rate-cache.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-curl -sf "rate.sx/1btc" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/btc-usd-rate"
--- dot_scripts/executable_update-cripto-rates.sh
+++ dot_scripts/executable_update-cripto-rates.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
+curl -sf "rate.sx/1btc" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/btc-usd-rate"
 curl -sf "rate.sx/1xlm" | awk '{ printf("$%.2f", $0) }' >"$HOME/.cache/xlm-usd-rate"
