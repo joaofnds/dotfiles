b8929e6284b3743042c4f91129a6d90e5b51db58
João Fernandes <joaofnds@joaofnds.com>
2022-09-08 11:22:57 -0300

    add ars-brl rate

--- dot_scripts/executable_update-rates.sh
+++ dot_scripts/executable_update-rates.sh
@@ -11,6 +11,8 @@ curl -s "http://api.exchangeratesapi.io/v1/latest?access_key=$EXCHANGERATESAPI_T
 BRL=$(jq -r '.rates.BRL' < "$RATESCACHE")
 USD=$(jq -r '.rates.USD' < "$RATESCACHE")
 EUR=$(jq -r '.rates.EUR' < "$RATESCACHE")
+ARS=$(jq -r '.rates.ARS' < "$RATESCACHE")
 
 bc -l <<< "1.0 / $EUR * $BRL" | awk '{ printf("R$%.2f", $0) }' > "$CACHEDIR/eur-brl-rate"
 bc -l <<< "1.0 / $USD * $BRL" | awk '{ printf("R$%.2f", $0) }' > "$CACHEDIR/usd-brl-rate"
+bc -l <<< "1.0 / $ARS * $BRL" | awk '{ printf("R$%.2f", $0) }' > "$CACHEDIR/ars-brl-rate"
