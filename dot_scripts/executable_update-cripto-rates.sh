ea59dc6bff4cf3de12121c74f48b3080a2fb4e75
João Fernandes <joaofnds@joaofnds.com>
2022-09-08 11:22:46 -0300

    add ethereum rate

--- dot_scripts/executable_update-cripto-rates.sh
+++ dot_scripts/executable_update-cripto-rates.sh
@@ -2,3 +2,4 @@
 
 curl -sf "rate.sx/1btc" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/btc-usd-rate"
 curl -sf "rate.sx/1xlm" | awk '{ printf("$%.2f", $0) }' >"$HOME/.cache/xlm-usd-rate"
+curl -sf "rate.sx/1eth" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/eth-usd-rate"
