357c1f4ce20f653749481e90527c9a4806f5cfe1
João Fernandes <joaofnds@joaofnds.com>
2022-09-12 18:24:35 -0300

    add script to send exchange rates to discord

---
+++ dot_scripts/executable_send-rates-to-discord.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+HOME=$(eval echo "~$joaofnds")
+CACHEDIR="$HOME/.cache"
+
+source "$HOME/.hardware.env"
+
+USD=$(cat "$CACHEDIR/usd-brl-rate")
+EUR=$(cat "$CACHEDIR/eur-brl-rate")
+ARS=$(cat "$CACHEDIR/ars-brl-rate")
+BTC=$(cat "$CACHEDIR/btc-usd-rate")
+ETH=$(cat "$CACHEDIR/eth-usd-rate")
+
+curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -d "{\"content\": \"USD: $USD\nEUR: $EUR\nARG: $ARS\nBTC: $BTC\nETH: $ETH\"}" \
+    "$PAI_RICO_WEBHOOK_URL"
