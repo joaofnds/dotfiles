84f6541a79b8fd134cd218f2d19efa5e146c5490
João Fernandes <joaofnds@joaofnds.com>
2022-10-04 15:49:55 -0300

    remove covid script

--- dot_scripts/executable_update-covid-cache.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-HOME=$(eval echo "~$joaofnds")
-
-curl -s https://corona.lmao.ninja/v2/countries/brazil |
-  jq '["💀", .deaths, "(", .todayDeaths, ") 🦠", .active, "(", .todayCases, ")"] | join("")' |
-  tr -d "\"\n" > $HOME/.cache/covid
