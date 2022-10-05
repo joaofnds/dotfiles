61058fe9bcdfdc60d5f2ad5a2c4df64d2c8e3805
João Fernandes <joaofnds@joaofnds.com>
2022-10-05 12:17:39 -0300

    add ail config

---
+++ dot_config/ail/hooks/executable_nodejs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+npm i -g typescript \
+         typescript-language-server \
+         yaml-language-server \
+         prettier \
+         snowpack \
+         lua-fmt \
+         dockerfile-language-server-nodejs \
+         @nestjs/cli \
+         vmd
---
+++ dot_config/ail/hooks/executable_python.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pip install --user black pyright pylint grip poetry
