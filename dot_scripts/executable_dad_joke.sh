68e376a852a0299c13e5752ace7970dc9a4477cc
João Fernandes <joaofnds@joaofnds.com>
2022-10-22 20:01:52 -0300

    add dad joke bot

---
+++ dot_scripts/executable_dad_joke.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+HOME=$(eval echo "~$joaofnds")
+source "$HOME/.hardware.env"
+
+JOKE=$(curl -s -H "X-Rapidapi-Key: $RAPIDAPI_KEY" -H "X-Rapidapi-Host: dad-jokes.p.rapidapi.com" https://dad-jokes.p.rapidapi.com/random/joke)
+
+setup=$(jq -r '.body[0].setup' <<<"$JOKE")
+punchline=$(jq -r '.body[0].punchline' <<<"$JOKE")
+
+curl -s -X POST \
+	-H "Content-Type: application/json" \
+	-d "{\"content\": \"$setup\"}" \
+	"$DADBOT_URL"
+
+sleep 5
+
+curl -s -X POST \
+	-H "Content-Type: application/json" \
+	-d "{\"content\": \"$punchline\"}" \
+	"$DADBOT_URL"
