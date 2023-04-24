c74a4188f3f166e87ab60e75dd76aec48dc1bfaa
João Fernandes <joaofnds@joaofnds.com>
2023-04-24 12:58:20 -0300

    use coinmarketcap to get cripto rates

--- dot_scripts/executable_update-cripto-rates.sh
+++ dot_scripts/executable_update-cripto-rates.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
-curl -sf "rate.sx/1btc" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/btc-usd-rate"
-curl -sf "rate.sx/1xlm" | awk '{ printf("$%.2f", $0) }' >"$HOME/.cache/xlm-usd-rate"
-curl -sf "rate.sx/1eth" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/eth-usd-rate"
+body=$(mktemp)
+
+curl --silent \
+	--output "$body" \
+	--header "X-CMC_PRO_API_KEY: $COINMARKETCAP_KEY" \
+	"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC,ETH,XLM"
+
+jq '.data.BTC.quote.USD.price' "$body" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/btc-usd-rate"
+jq '.data.ETH.quote.USD.price' "$body" | awk '{ printf("$%.1fk", $0/1000) }' >"$HOME/.cache/eth-usd-rate"
+jq '.data.XLM.quote.USD.price' "$body" | awk '{ printf("$%.2f", $0) }' >"$HOME/.cache/xlm-usd-rate"
