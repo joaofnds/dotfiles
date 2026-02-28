2a90b3b8ad0d3e7fe47ab1dce66c7f9a0325cf85
João Fernandes <joaofnds@joaofnds.com>
2026-02-28 20:41:44 -0300

    switch theme to maxmx03/solarized.nvim

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,23 +1,12 @@
 return {
-	"ishan9299/nvim-solarized-lua",
-	event = "UIEnter",
+	"maxmx03/solarized.nvim",
+	lazy = false,
+	priority = 1000,
+	---@type solarized.config
+	opts = {},
 	init = function()
+		vim.o.termguicolors = true
+		vim.o.background = "dark"
 		vim.cmd.colorscheme("solarized")
-		vim.api.nvim_set_hl(0, "Visual", { bg = "#073642", fg = "NONE" })
-		vim.api.nvim_set_hl(0, "VisualNOS", { bg = "#073642", fg = "NONE" })
-		vim.api.nvim_set_hl(0, "VisualMode", { bg = "#073642", fg = "NONE" })
-		vim.api.nvim_set_hl(0, "Search", {
-			bg = "#073642",
-			fg = "NONE",
-			undercurl = true,
-			italic = true,
-		})
-		vim.api.nvim_set_hl(0, "IncSearch", {
-			bg = "#073642",
-			fg = "NONE",
-			undercurl = true,
-			italic = true,
-		})
-		vim.api.nvim_set_hl(0, "LeapLabel", { fg = "#d33682" })
 	end,
 }
