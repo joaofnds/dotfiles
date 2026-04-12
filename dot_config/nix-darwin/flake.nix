c05baf71a9e9d26d183caff7efb1a4501894c044
João Fernandes <joaofnds@joaofnds.com>
2026-04-11 23:49:32 -0300

    add nix-darwin config for mbp-16

--- dot_config/nix-darwin/flake.nix
+++ dot_config/nix-darwin/flake.nix
@@ -21,29 +21,37 @@
         {
           nix.enable = false;
           nix.settings.experimental-features = "nix-command flakes";
-          nixpkgs.hostPlatform = "aarch64-darwin";
           programs.zsh.enable = true;
           security.pam.services.sudo_local.touchIdAuth = true;
           system.configurationRevision = self.rev or self.dirtyRev or null;
           system.primaryUser = "joaofnds";
           system.stateVersion = 6;
         };
+      common = [
+        configuration
+        ./system-defaults.nix
+        ./packages.nix
+        ./homebrew.nix
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            autoMigrate = true;
+            enableRosetta = false;
+            user = "joaofnds";
+          };
+        }
+      ];
     in
     {
       darwinConfigurations."joaofnds-m1p" = nix-darwin.lib.darwinSystem {
-        modules = [
-          configuration
-          ./system-defaults.nix
-          ./packages.nix
-          ./homebrew.nix
-          nix-homebrew.darwinModules.nix-homebrew
-          {
-            nix-homebrew = {
-              enable = true;
-              enableRosetta = false;
-              user = "joaofnds";
-            };
-          }
+        modules = common ++ [
+          { nixpkgs.hostPlatform = "aarch64-darwin"; }
+        ];
+      };
+      darwinConfigurations."joaofnds-mbp-16" = nix-darwin.lib.darwinSystem {
+        modules = common ++ [
+          { nixpkgs.hostPlatform = "x86_64-darwin"; }
         ];
       };
     };
