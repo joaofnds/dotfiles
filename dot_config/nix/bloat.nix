6c44d8cd7c98541cac00af4aa44d5dcb757158bf
João Fernandes <joaofnds@joaofnds.com>
2026-06-02 14:01:04 +0200

    move default lang packages

--- dot_config/nix/bloat.nix
+++ dot_config/nix/bloat.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [
+    pkgs.difftastic
+    pkgs.gitu
+    pkgs.go-grip
     pkgs.shfmt
+    pkgs.sqruff
     pkgs.texliveFull
     pkgs.yt-dlp
   ];
